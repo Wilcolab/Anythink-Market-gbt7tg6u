138557c2ba6220370a234b551772640cbddaca1c
Yosef Alemayehu <yalenayehu2123@gmail.com>
2023-11-28 22:44:04 +0000

    populate a database

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,2 +1,13 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
-//i
+const faker = require('faker');
+
+const title = faker.name.title();
+const description = faker.name.description();
+
+const jsonData = JSON.stringify({
+  title,
+  description,
+   });
+   console.log(jsonData);
+
+   faker.seed(100);
