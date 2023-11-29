d4742781daadef45e99b30d7923998e6b1d273a6
Yosef Alemayehu <yalenayehu2123@gmail.com>
2023-11-29 19:22:37 +0000

    seed script

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,12 +1,14 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
 const faker = require('faker');
 
-const title = faker.name.title();
-const description = faker.name.description();
+const users = faker.internet.users();
+const product = faker.commerce.product();
+const Comment = faker.name.Comment();
 
 const jsonData = JSON.stringify({
-  title,
-  description,
+  users,
+  product,
+  Comment,
    });
    console.log(jsonData);
 
