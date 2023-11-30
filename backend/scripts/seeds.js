071f01a15c1d421b94a11430b40f6c8e0c0cd8d3
Yosef Alemayehu <yalenayehu2123@gmail.com>
2023-11-30 19:10:33 +0000

    bb

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,15 +1,21 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
-const faker = require('faker');
+var faker = require("faker");
 
-const users = faker.internet.users();
-const product = faker.commerce.product();
-const Comment = faker.name.Comment();
+var randomUsers = faker.internet.userName() ;
+var randomProduct = faker.commerce.productName(); 
+var randomComment = faker.name.Comment();
 
-const jsonData = JSON.stringify({
-  users,
-  product,
-  Comment,
-   });
-   console.log(jsonData);
+// const jsonData = JSON.stringify({
+//   users,
+//   product,
+//   Comment,
+//    });
+//    console.log(jsonData);
 
-   faker.seed(100);
+//    faker.seed(100);
+
+for(i = 0; i <= 100; i++) {
+  console.log(andomUsers);
+  console.log(randomProduct);
+  console.log(randomComment);
+};
