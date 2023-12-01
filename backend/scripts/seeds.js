a8687fbd67a8381a780ea4abd0fe9d4338c3ad61
Yosef Alemayehu <yalenayehu2123@gmail.com>
2023-12-01 20:13:48 +0000

    bb

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,17 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
+const faker = require ('faker');
+
+app.get("/users", (req, res, next) => {
+    faker.seed(100);
+    res.json(getUsers());
+});
+
+app.get("/products", (req, res, next) => {
+    faker.seed(100);
+    res.json(Products());
+});
+
+app.get("/comments", (req, res, next) => {
+    faker.seed(100);
+    res.json(getComments());
+});
