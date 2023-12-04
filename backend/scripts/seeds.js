244ecbfab60cbb907b1ff749708bbece64d08d3c
Yosef Alemayehu <yalenayehu2123@gmail.com>
2023-12-04 23:50:01 +0000

    1

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,21 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
+import Chance from "chance";
+const chanceObj = new Chance();
+
+const generatePerson = () => {
+    return {
+        "users": chanceObj.users(),
+        "products": chanceObj.provinces(),
+        "comments": chanceObj.comments(),
+    };
+};
+
+const  dom = generatePerson();
+console.log(dom);
+
+// var Chance = require ('chance');
+// var chance = new Chance();
+
+// console.log(chance.integer());
+// console.log(chance.products(100));
+// console.log(chance.comments(100));
