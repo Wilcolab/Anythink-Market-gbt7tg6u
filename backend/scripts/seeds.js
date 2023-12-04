5962e03397d34074c8207fb811fc31c29bad51ca
Yosef Alemayehu <yalenayehu2123@gmail.com>
2023-12-04 21:21:15 +0000

    YM

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,14 +1,14 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
-const faker = require('faker');
+import { helpers, commerce, lorem } from 'faker';
 
 
-const userData = faker.helpers.contextualCard();
-const product = faker.commerce.product();
-const fakeComment = faker.lorem.sentence();
+const userData = helpers.contextualCard();
+const product = commerce.product();
+const fakeComment = lorem.sentence();
 
 
 for (i = 0; i < 100; i++) {
-	console.log(ruserData); // Outputs a random name
+	console.log(userData); // Outputs a random name
 	console.log(product); // Outputs a random email
 	console.log(fakeComment); // Outputs the random product name generated
 	
