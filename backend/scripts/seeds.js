5e970d54f98cab3079abb7c0c7caeda3480aaf88
Yosef Alemayehu <yalenayehu2123@gmail.com>
2023-12-11 22:10:30 +0000

    modul

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,7 +1,7 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
 var Chance = require('chance');
 var chance = new Chance();
-
+// i did
 
 function main() {
     Array.from( { length:100 }).map(async (i) => {
