4df521cda13bcf8606613adb04c1fade315bd8d3
Yosef Alemayehu <yalenayehu2123@gmail.com>
2023-12-11 22:12:36 +0000

    path

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,5 +1,5 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
-var Chance = require('chance');
+var Chance = require('/workspaces/Anythink-Market-gbt7tg6u/node_modules/chance/chance.js');
 var chance = new Chance();
 // i did
 
