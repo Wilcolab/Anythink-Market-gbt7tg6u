1444b819a91731238736ba8e27c18db618b04847
Yosef Alemayehu <yalenayehu2123@gmail.com>
2023-12-11 21:06:16 +0000

    seed a data

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,17 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
+import { faker } from'@faker-js/faker';
+const faker = new Faker();
+
+function main() {
+    Array.from( { length:100 }).map(async (i) => {
+        await faker.user.create({
+            data: {
+                user: faker.name.user(),
+                product: faker.commerce.product,
+                comment: faker.lorem.comment,
+            },
+        });
+    });
+}
+
+main();
