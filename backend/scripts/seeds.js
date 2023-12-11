5c41419c87ccfad4eed9954c339af9871f733775
Yosef Alemayehu <yalenayehu2123@gmail.com>
2023-12-11 22:03:52 +0000

    var

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,5 +1,5 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
-import Chance from 'chance';
+var Chance = require('chance');
 var chance = new Chance();
 
 
