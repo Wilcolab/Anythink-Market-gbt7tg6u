93232e2b33f88d3fe14cc6ece0559ad4f8f569b8
Yosef Alemayehu <yalenayehu2123@gmail.com>
2023-12-11 21:32:40 +0000

    4

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,5 +1,5 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
-var Faker = require('./node_modules/Faker');
+var faker = require('./node_modules/faker');
 // const faker = new Faker();
 
 function main() {
